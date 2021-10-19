5f00c2cb995b9f2d43851bf09ec2e2de7c902d54
30-DarenOng <91870563+30-DarenOng@users.noreply.github.com>
2021-10-19 15:57:44 +0800

    Implemented Flying
    SEGGS

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,16 +5,37 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     private Animation thisAnimation;
+    private Rigidbody rb;
+    public Vector3 jump;
+    public float jumpForce;
 
     void Start()
     {
         thisAnimation = GetComponent<Animation>();
         thisAnimation["Flap_Legacy"].speed = 3;
+        rb = GetComponent<Rigidbody>();
+
     }
 
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Space))
+        {
             thisAnimation.Play();
+            rb.AddForce(jump * jumpForce, ForceMode.Impulse);
+
+        }    
+           
+
+
+
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Obstacle")
+        {
+            Destroy(gameObject);
+        }
+       
     }
 }
